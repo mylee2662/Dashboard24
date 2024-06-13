64b4ed81794a98ff73890330095106c86cccc146
Shrimp-Chip <esample21@gmail.com>
2024-06-13 08:59:22 -0400

    Bars

--- src/dash.cpp
+++ src/dash.cpp
@@ -1,5 +1,6 @@
 #include "dash.h"
 
+#include <iostream>
 #include <cmath>
 #include <string>
 #include "teensy_can.h"
@@ -17,6 +18,8 @@
 #define SCREEN_HEIGHT 480
 #define CENTER SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2
 #define CENTER_OFFSET(x, y) SCREEN_WIDTH / 2 + x, SCREEN_HEIGHT / 2 + y
+#define BAR_HEIGHT SCREEN_HEIGHT
+#define BAR_WIDTH 60
 
 int drive_state_startX = SCREEN_WIDTH / 2;
 int drive_state_startY = SCREEN_HEIGHT / 2 - 160;
@@ -26,8 +29,6 @@ int bar_max_size = 480;
 
 #define DEBUG
 
-// float fl_wheel_speed;
-// float fr_wheel_speed;
 float motor_temp;
 int drive_state = -1;
 bool drive_state_drawn = false;
@@ -54,10 +55,15 @@ void Dash::Initialize()
     pinMode(IMD_ERR_PIN, OUTPUT);
     // ake the pin low
     digitalWrite(IMD_ERR_PIN, LOW);
+
+    // create bars
+    this->bars["test"] = BarData("t", 0, 100, 0, SCREEN_HEIGHT, BAR_WIDTH, BAR_HEIGHT);
+    this->bars["test2"] = BarData("23", 0, 100, BAR_WIDTH + 20, SCREEN_HEIGHT, BAR_WIDTH, BAR_HEIGHT);
 }
 
 void Dash::DrawBackground(Adafruit_RA8875 tft, int16_t color)
 {
+    this->backgroundColor = color;
     // black out the screen
     tft.fillScreen(color);
 
@@ -65,17 +71,17 @@ void Dash::DrawBackground(Adafruit_RA8875 tft, int16_t color)
     int rect_height = 200;
     int rect_border_height = rect_height + 2 * border;
     // draw outlines
-    tft.fillRect(0, SCREEN_HEIGHT / 2 - rect_border_height / 2, SCREEN_WIDTH, rect_border_height, RA8875_BLACK);
+    // tft.fillRect(0, SCREEN_HEIGHT / 2 - rect_border_height / 2, SCREEN_WIDTH, rect_border_height, RA8875_BLACK);
     tft.fillCircle(CENTER, SCREEN_HEIGHT / 2, RA8875_BLACK);
     // fill in
-    tft.fillRect(0, SCREEN_HEIGHT / 2 - rect_height / 2, SCREEN_WIDTH, rect_height, RA8875_WHITE);
+    // tft.fillRect(0, SCREEN_HEIGHT / 2 - rect_height / 2, SCREEN_WIDTH, rect_height, RA8875_WHITE);
     tft.fillCircle(CENTER, SCREEN_HEIGHT / 2 - border, RA8875_WHITE);
 
-    // to the left, center, draw "go"
-    this->DrawString(tft, "GO", CENTER_OFFSET(-350, -30), 8, RA8875_BLACK);
+    // // to the left, center, draw "go"
+    // this->DrawString(tft, "GO", CENTER_OFFSET(-350, -30), 8, RA8875_BLACK);
 
-    // to the right, center, draw "CATS"
-    this->DrawString(tft, "CATS", CENTER_OFFSET(200, -30), 8, RA8875_BLACK);
+    // // to the right, center, draw "CATS"
+    // this->DrawString(tft, "CATS", CENTER_OFFSET(200, -30), 8, RA8875_BLACK);
 }
 
 float Dash::WheelSpeedAvg(float fl_wheel_speed, float fr_wheel_speed)
@@ -110,45 +116,58 @@ void Dash::UpdateDisplay(Adafruit_RA8875 tft)
     // draw IMD status
     DrawIMDStatus(tft, 8, 8, imd_status, 32);
 
+    // draw the test bar
+    this->DrawBar(tft, "test", (millis() / 100) % 100 , RA8875_GREEN, this->backgroundColor);
+    this->DrawBar(tft, "test2", (millis() / 20) % 100, RA8875_RED, this->backgroundColor);
+
     timer_group.Tick(millis());
 }
 
-void Dash::DrawBar(Adafruit_RA8875 tft, int startX, int *prev_bar_y, int width, float value, int min_value, int max_value, int *prev_bar_level)
+void Dash::DrawBar(Adafruit_RA8875 tft, std::string barName, float newValue, int16_t barColor, int16_t backgroundColor)
 {
-    // Check the bounds for any potential issues
-    if (value < min_value)
+    // get the bar if any
+    if (this->bars.find(barName) == this->bars.end())
     {
-        Serial.println("Value is below minimum");
-    }
-    else if (value > max_value)
-    {
-        Serial.println("Value is above maximum");
+        Serial.println("Bar not found");
+        return;
     }
 
-    // White out space
-    // tft.fillRect(startX, startY, width, bar_max_size, RA8875_WHITE);
+    BarData& bar = this->bars[barName];
 
-    // Calculate the size of the current bar with respect to its max size percentage wise
-    float curr_bar_percentage = (value - min_value) / (max_value - min_value);
-    int curr_bar_level = round(curr_bar_percentage * bar_max_size); // Get the actual size of the current bar
-    // int bar_size_diff = bar_max_size - curr_bar_level;
-    // int curr_bar_y = startY + bar_size_diff;
-
-    int bar_level_diff = curr_bar_level - *prev_bar_level; // negative means the bar is shrinking (draw white space), positive means bar is growing (draw bar)
-
-    if (bar_level_diff < 0)
+    // are the values the same?
+    if (bar.value == newValue)
     {
-        // white out space
-        tft.fillRect(startX, *prev_bar_y, width, bar_level_diff, RA8875_WHITE);
-    }
-    else if (bar_level_diff > 0)
-    {
-        // Draw additional bar segment
-        tft.fillRect(startX, *prev_bar_level + bar_level_diff, width, bar_level_diff, RA8875_BLUE);
+        return;
     }
 
-    *prev_bar_level = curr_bar_level;
-    *prev_bar_y += bar_level_diff;
+    // calculate the height of the bar
+    int newHeight = CalcBarHeight(newValue, bar.min, bar.max, bar.maxHeight);
+    int oldHeight = CalcBarHeight(bar.value, bar.min, bar.max, bar.maxHeight);
+
+    if (newHeight == oldHeight)
+    {
+        return;
+    }
+
+    // draw the bar
+    int diff = newHeight - oldHeight;
+    bar.value = newValue;
+
+    std::cout << "Drawing bar " << barName << " with value " << newValue << " and height " << newHeight << std::endl;
+
+    if (diff > 0)
+    {
+        // if the new height is greater than the old height, we need to fill in the difference
+        // we will draw the bar to go upwards
+        // the top left of the screen is 0,0
+        tft.fillRect(bar.x, bar.y - newHeight, bar.width, diff, barColor);
+    }
+    else
+    {
+        // if the new height is less than the old height, we need to clear the difference
+        // we will draw the bar to go downwards
+        tft.fillRect(bar.x, bar.y - oldHeight, bar.width, -diff, backgroundColor);
+    }
 }
 
 void Dash::DrawWheelSpeed(Adafruit_RA8875 tft, float wheel_speed, int startX, int startY)
@@ -212,7 +231,6 @@ void Dash::DrawDriveState(Adafruit_RA8875 tft, int startX, int startY, int curr_
 
     tft.drawChar(startX - 32, startY, state, RA8875_BLACK, RA8875_WHITE, 16);
     drive_state = curr_drive_state;
-
 }
 
 void Dash::DrawIMDStatus(Adafruit_RA8875 tft, int startX, int startY, int imd_status, int squareSize)
@@ -250,10 +268,27 @@ void Dash::DrawError(Adafruit_RA8875 tft, std::string error_message, int startX,
     DrawString(tft, error_message, startX, startY, 5, RA8875_WHITE);
 }
 
-void Dash::DrawString(Adafruit_RA8875 tft, std::string message, int startX, int startY, int size, int color)
+void Dash::DrawString(Adafruit_RA8875 tft, std::string message, int startX, int startY, int size, int color, Direction dir)
 {
     for (int i = 0; i < message.length(); i++)
     {
-        tft.drawChar(startX + i * size * 6, startY, message[i], RA8875_BLACK, RA8875_WHITE, size);
+        switch (dir)
+        {
+        case LEFT_TO_RIGHT:
+            tft.drawChar(startX + i * size * 6, startY, message[i], RA8875_BLACK, RA8875_WHITE, size);
+            break;
+        case UP_TO_DOWN:
+            tft.drawChar(startX, startY + i * size * 8, message[i], RA8875_BLACK, RA8875_WHITE, size);
+            break;
+        default:
+            break;
+        }
     }
+}
+
+int Dash::CalcBarHeight(float value, float min, float max, int maxHeight)
+{
+    int lerp = (value - min) / (max - min) * maxHeight;
+    // clamp the value between 0 and maxHeight
+    return lerp > maxHeight ? maxHeight : lerp < 0 ? 0 : lerp;
 }
